b59442d10753e594654d13a86ef183a9b4373da8
Zak <zachary.strassberg@abbvie.com>
2020-11-18 09:28:38 -0800

    build

--- packages/reakit/src/Search/__keys.ts
+++ packages/reakit/src/Search/__keys.ts
@@ -1,2 +1,15 @@
-export const SEARCH_INPUT_KEYS = [] as const;
-export const SEARCH_DISCLOSURE_KEYS = [] as const;
+// Automatically generated
+const SEARCH_STATE_KEYS = [
+  "baseId",
+  "unstable_idCountRef",
+  "visible",
+  "setBaseId",
+  "show",
+  "hide",
+  "setVisible",
+] as const;
+export const SEARCH_DISCLOSURE_KEYS = [
+  ...SEARCH_STATE_KEYS,
+  "unstable_system",
+] as const;
+export const SEARCH_INPUT_KEYS = SEARCH_DISCLOSURE_KEYS;
